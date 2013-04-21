acc9be58110031db4c1194fa22ba03b2e3ef030a
Max Bernstein <tekknolagi@gmail.com>
2013-04-21 23:47:30 +0000

    coinbase

--- app.rb
+++ app.rb
@@ -1,7 +1,15 @@
 require 'sinatra'
+require 'coinbase'
 
 class Banana < Sinatra::Application
+  coinbase = Coinbase::Client.new(ENV['COINBASE_API_KEY'])
+
   get '/' do
-    "I AM THE MOTHERFUCKING BANANA LORD"
+    erb :index
+  end
+
+  get '/our_balance' do
+    coinbase.balance.to_f
   end
 end
+
