654a2ef9f0517ce55ab5c58b7c7bc312f62166d3
chinstorff <christopherhinstorff@me.com>
2013-04-22 03:54:43 +0000

    donate added

--- app.rb
+++ app.rb
@@ -10,8 +10,7 @@ end
 class Banana < Sinatra::Application
   configure do
     set :coinbase, Coinbase::Client.new(ENV['COINBASE_API_KEY'])
-    set :quantity_regex, /^([0-9])+$/
-    set :email_regex, /^([0-9][A-Z][a-z])+\@([0-9][A-Z][a-z])+\.([A-Z][a-z])+$/
+    set :email_regex, /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}/
   end
 
   get '/' do
@@ -19,16 +18,13 @@ class Banana < Sinatra::Application
   end
 
   post '/request' do
-    unless params[:quantity].validate(settings.quantity_regex)
-      @quantity_error = "Your 'quantity' field contained invalid data (#{params[:quantity]})."
-    end
     unless params[:receiving].validate(settings.email_regex)
       @receiving_error = "Your 'receiving email' field contained invalid data (#{params[:receiving]})."
     end
     unless params[:sending].validate(settings.email_regex)
       @sending_error = "Your 'sending email' field contained invalid data (#{params[:sending]})."
     end
-    if @quantity_error || @receiving_error || @sending_error
+    if @receiving_error || @sending_error
       erb :error
     else
       erb :request
