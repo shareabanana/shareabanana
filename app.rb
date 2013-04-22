1316c35057639b8f541db8679318ed43028286a0
Max Bernstein <tekknolagi@gmail.com>
2013-04-22 00:18:29 +0000

    class var

--- app.rb
+++ app.rb
@@ -2,14 +2,14 @@ require 'sinatra'
 require 'coinbase'
 
 class Banana < Sinatra::Application
-  coinbase = Coinbase::Client.new(ENV['COINBASE_API_KEY'])
+  @coinbase = Coinbase::Client.new(ENV['COINBASE_API_KEY'])
 
   get '/' do
     erb :index
   end
 
   get '/balance' do
-    return coinbase.balance.to_f
+    return @coinbase.balance.to_f
   end
 end
 
